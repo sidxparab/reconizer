63330b1bc027ab4898d3becee48317d863a7178e
Siddhesh Parab <64590875+sidxparab@users.noreply.github.com>
2022-08-31 01:53:50 +0530

    Added common ports probing

--- reconizer.sh
+++ reconizer.sh
@@ -15,6 +15,8 @@ STD_ERR="2>/dev/null"
 tools=~/Tools
 subdomains_list=$tools/sub_brute_small.txt
 GOTATOR_TIMEOUT="timeout 100"
+COMMON_WEB_PORTS="81,300,591,593,832,981,1010,1311,1099,2082,2095,2096,2480,3000,3128,3333,4243,4567,4711,4712,4993,5000,5104,5108,5280,5281,5601,5800,6543,7000,7001,7396,7474,8000,8001,8008,8014,8042,8060,8069,8080,8081,8083,8088,8090,8091,8095,8118,8123,8172,8181,8222,8243,8280,8281,8333,8337,8443,8500,8834,8880,8888,8983,9000,9001,9043,9060,9080,9090,9091,9092,9200,9443,9502,9800,9981,10000,10250,11371,12443,15672,16080,17778,18091,18092,20720,32000,55440,55672"
+
 
 banner(){
 printf "\n${green}"
@@ -236,11 +238,21 @@ subdomain_takeover()
 web_probing()
 {
 	mkdir -p web/
-	printf "${yellow}Web probing started${reset}\n\n"
+	printf "${yellow}Web probing on standard ports started${reset}\n\n"
 	eval httpx -retries 2 -silent -timeout 10 -l subdomains/subdomains.txt -o .tmp/web_probed_tmp.txt $STD_OUT
 	Number_of_lines=$(cat .tmp/web_probed_tmp.txt | anew web/webs.txt  | wc -l )
 	printf "${green}Found!!: $Number_of_lines New Websites${reset}\n\n"
-	printf "${yellow}Web probing Ended${reset}\n"
+	printf "${yellow}}Web probing on standard ports Ended${reset}\n"
+	printf "${green}##############################################################################${reset}\n\n"
+}
+
+web_probing_common()
+{
+	printf "${yellow}Web probing on common ports started${reset}\n\n"
+	eval httpx -silent -p $COMMON_WEB_PORTS -l subdomains/subdomains.txt -o .tmp/web_probed_tmp.txt $STD_OUT
+	Number_of_lines=$(cat .tmp/web_probed_tmp.txt | anew web/webs.txt  | wc -l )
+	printf "${green}Found!!: $Number_of_lines New Websites${reset}\n\n"
+	printf "${yellow}Web probing on common ports ended${reset}\n"
 	printf "${green}##############################################################################${reset}\n\n"
 }
 
@@ -323,6 +335,7 @@ while getopts ":d:o:snahc" opt;do
 			subdomain_analytics
 			subdomain_takeover
 			web_probing
+			web_probing_common
 			web_screenshot
 			;;
 		o ) output_folder=$OPTARG
@@ -344,6 +357,7 @@ while getopts ":d:o:snahc" opt;do
 			subdomain_analytics
 			subdomain_takeover
 			web_probing
+			web_probing_common
 			web_screenshot
 			;;
 		a )
@@ -359,6 +373,7 @@ while getopts ":d:o:snahc" opt;do
 			subdomain_analytics
 			subdomain_takeover
 			web_probing
+			web_probing_common
 			web_screenshot
 			;;
 		c )
